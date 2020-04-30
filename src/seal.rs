230e93cfcf628abc7ad3ef01a4a5e7cc0f7a22fb
石博文 <sbw@sbw.so>
2020-04-30 14:22:34 +0800

    add seal interfaces

--- src/seal.rs
+++ src/seal.rs
@@ -1,11 +1,10 @@
-use std::path::Path;
+use crate::seal_data::*;
 
 use actix_web::web::Json;
 use actix_web::{HttpRequest, HttpResponse};
 use filecoin_proofs_api::{seal, PieceInfo};
 use log::trace;
-
-use crate::seal_data::*;
+use std::path::Path;
 
 pub async fn clear_cache(_req: HttpRequest, data: Json<ClearCacheData>) -> HttpResponse {
     trace!("clear_cache");
@@ -18,7 +17,7 @@ pub async fn clear_cache(_req: HttpRequest, data: Json<ClearCacheData>) -> HttpR
 pub async fn seal_pre_commit_phase1(data: Json<SealPreCommitPhase1Data>) -> HttpResponse {
     trace!("seal_pre_commit_phase1");
 
-    let piece_info: Vec<PieceInfo> = data.piece_infos.iter().map(|x| x.as_object()).collect();
+    let piece_infos: Vec<PieceInfo> = data.piece_infos.iter().map(|x| x.as_object()).collect();
 
     let r = seal::seal_pre_commit_phase1(
         data.registered_proof,
@@ -28,7 +27,107 @@ pub async fn seal_pre_commit_phase1(data: Json<SealPreCommitPhase1Data>) -> Http
         data.prover_id,
         data.sector_id,
         data.ticket,
-        &piece_info[..],
+        &piece_infos[..],
+    );
+
+    HttpResponse::Ok().json(r.map_err(|e| format!("{:?}", e)))
+}
+
+pub async fn seal_pre_commit_phase2(data: Json<SealPreCommitPhase2Data>) -> HttpResponse {
+    trace!("seal_pre_commit_phase2");
+
+    let r = seal::seal_pre_commit_phase2(data.phase1_output.clone(), &data.cache_path, &data.out_path);
+
+    HttpResponse::Ok().json(r.map_err(|e| format!("{:?}", e)))
+}
+
+pub async fn compute_comm_d(data: Json<ComputeCommDData>) -> HttpResponse {
+    trace!("compute_comm_d");
+
+    let piece_infos: Vec<PieceInfo> = data.piece_infos.iter().map(|x| x.as_object()).collect();
+
+    let r = seal::compute_comm_d(data.registered_proof, &piece_infos[..]);
+
+    HttpResponse::Ok().json(r.map_err(|e| format!("{:?}", e)))
+}
+
+pub async fn seal_commit_phase1(data: Json<SealCommitPhase1Data>) -> HttpResponse {
+    trace!("seal_commit_phase1");
+
+    let piece_infos: Vec<PieceInfo> = data.piece_infos.iter().map(|x| x.as_object()).collect();
+
+    let r = seal::seal_commit_phase1(
+        &data.cache_path,
+        &data.replica_path,
+        data.prover_id,
+        data.sector_id,
+        data.ticket,
+        data.seed,
+        data.pre_commit.clone(),
+        &piece_infos[..],
+    );
+
+    HttpResponse::Ok().json(r.map_err(|e| format!("{:?}", e)))
+}
+
+pub async fn seal_commit_phase2(data: Json<SealCommitPhase2Data>) -> HttpResponse {
+    trace!("seal_commit_phase2");
+
+    let r = seal::seal_commit_phase2(data.phase1_output.clone(), data.prover_id, data.sector_id);
+
+    HttpResponse::Ok().json(r.map_err(|e| format!("{:?}", e)))
+}
+
+pub async fn verify_seal(data: Json<VerifySealData>) -> HttpResponse {
+    trace!("verify_seal");
+
+    let r = seal::verify_seal(
+        data.registered_proof,
+        data.comm_r_in,
+        data.comm_d_in,
+        data.prover_id,
+        data.sector_id,
+        data.ticket,
+        data.seed,
+        data.proof_vec.as_slice(),
+    );
+
+    HttpResponse::Ok().json(r.map_err(|e| format!("{:?}", e)))
+}
+
+pub async fn verify_batch_seal(data: Json<VerifyBatchSealData>) -> HttpResponse {
+    trace!("verify_batch_seal");
+
+    let proof_vecs: Vec<_> = data.proof_vecs.iter().map(|x| x.as_slice()).collect();
+
+    let r = seal::verify_batch_seal(
+        data.registered_proof,
+        data.comm_r_ins.as_slice(),
+        data.comm_d_ins.as_slice(),
+        data.prover_ids.as_slice(),
+        data.sector_ids.as_slice(),
+        data.tickets.as_slice(),
+        data.seeds.as_slice(),
+        proof_vecs.as_slice(),
+    );
+
+    HttpResponse::Ok().json(r.map_err(|e| format!("{:?}", e)))
+}
+
+pub async fn get_unsealed_range(data: Json<GetUnsealedRangeData>) -> HttpResponse {
+    trace!("get_unsealed_range");
+
+    let r = seal::get_unsealed_range(
+        data.registered_proof,
+        &data.cache_path,
+        &data.sealed_path,
+        &data.output_path,
+        data.prover_id,
+        data.sector_id,
+        data.comm_d,
+        data.ticket,
+        data.offset,
+        data.num_bytes,
     );
 
     HttpResponse::Ok().json(r.map_err(|e| format!("{:?}", e)))
--- src/seal_data.rs
+++ src/seal_data.rs
@@ -1,7 +1,10 @@
-use filecoin_proofs_api::{ProverId, RegisteredSealProof, SectorId, Ticket};
-use serde::Deserialize;
-
 use crate::types::*;
+
+use filecoin_proofs_api::seal::{SealCommitPhase1Output, SealPreCommitPhase1Output, SealPreCommitPhase2Output};
+use filecoin_proofs_api::{
+    Commitment, ProverId, RegisteredSealProof, SectorId, Ticket, UnpaddedByteIndex, UnpaddedBytesAmount,
+};
+use serde::Deserialize;
 
 #[derive(Deserialize, Clone, Debug)]
 pub struct ClearCacheData {
@@ -20,3 +23,79 @@ pub struct SealPreCommitPhase1Data {
     pub ticket: Ticket,
     pub piece_infos: Vec<WebPieceInfo>,
 }
+
+#[derive(Deserialize, Clone, Debug)]
+pub struct SealPreCommitPhase2Data {
+    pub phase1_output: SealPreCommitPhase1Output,
+    pub cache_path: String,
+    pub out_path: String,
+}
+
+#[derive(Deserialize, Clone, Debug)]
+pub struct ComputeCommDData {
+    pub registered_proof: RegisteredSealProof,
+    pub piece_infos: Vec<WebPieceInfo>,
+}
+
+#[derive(Deserialize, Clone, Debug)]
+pub struct SealCommitPhase1Data {
+    pub cache_path: String,
+    pub replica_path: String,
+    pub prover_id: ProverId,
+    pub sector_id: SectorId,
+    pub ticket: Ticket,
+    pub seed: Ticket,
+    pub pre_commit: SealPreCommitPhase2Output,
+    pub piece_infos: Vec<WebPieceInfo>,
+}
+
+#[derive(Deserialize, Clone, Debug)]
+pub struct SealCommitPhase2Data {
+    pub phase1_output: SealCommitPhase1Output,
+    pub prover_id: ProverId,
+    pub sector_id: SectorId,
+}
+
+#[derive(Deserialize, Clone, Debug)]
+pub struct VerifySealData {
+    pub registered_proof: RegisteredSealProof,
+    pub comm_r_in: Commitment,
+    pub comm_d_in: Commitment,
+    pub prover_id: ProverId,
+    pub sector_id: SectorId,
+    pub ticket: Ticket,
+    pub seed: Ticket,
+    pub proof_vec: Vec<u8>,
+}
+
+#[derive(Deserialize, Clone, Debug)]
+pub struct VerifyBatchSealData {
+    pub registered_proof: RegisteredSealProof,
+    pub comm_r_ins: Vec<Commitment>,
+    pub comm_d_ins: Vec<Commitment>,
+    pub prover_ids: Vec<ProverId>,
+    pub sector_ids: Vec<SectorId>,
+    pub tickets: Vec<Ticket>,
+    pub seeds: Vec<Ticket>,
+    pub proof_vecs: Vec<Vec<u8>>,
+}
+
+#[derive(Deserialize, Clone, Debug)]
+pub struct GetUnsealedRangeData {
+    pub registered_proof: RegisteredSealProof,
+    pub cache_path: String,
+    pub sealed_path: String,
+    pub output_path: String,
+    pub prover_id: ProverId,
+    pub sector_id: SectorId,
+    pub comm_d: Commitment,
+    pub ticket: Ticket,
+    pub offset: UnpaddedByteIndex,
+    pub num_bytes: UnpaddedBytesAmount,
+}
+
+// #[derive(Deserialize, Clone, Debug)]
+// pub struct GeneratePieceCommitmentData {
+//     pub registered_proof: RegisteredSealProof,
+//     pub source:
+// }
